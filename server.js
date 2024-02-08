e60b766de124375f1bc59e927067d80f4c90407f
MambrosePersonal <mr.mikeambrose@gmail.com>
2024-02-08 09:03:41 -0500

    React App + Express Server

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import { MongoClient } from 'mongodb';
+
+
+
+var url = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(url);
+const dbName = "";
+
+const app = express();
+
+app.use(express.json());
+
+
+
+app.all("*", (req, res, next) => {
+  console.log(req.url);
+  next();
+});
+
+app.get("/api/", async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection('');
+    const data = await collection.find({}).toArray();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+const port = 3500;
+client.close();
+app.listen(port, () => console.log(`Listening on port ${port}.`));
